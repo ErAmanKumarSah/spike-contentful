7901b795488c6ff3a5823be0886508b5e141ddc1
AmanKumar <amankumar.m@madrasglobal.com>
2017-05-10 18:45:13 +0530

    spike contentful project added

---
+++ app.js
@@ -0,0 +1,44 @@
+const SpikeUtils = require('spike-util')
+const Contentful = require('spike-contentful')
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const pageId = require('spike-page-id')
+const locals = {}
+module.exports = {
+  plugins: [
+    new Contentful({
+      addDataTo: locals,
+      accessToken: 'b2a023ce0af8f50240dce5cdfd5f8a9ab298f93bcaf24c57a9ab98e5191845c0',
+      spaceId: 'pa6kchr1mcxx',
+      contentTypes: [
+        {
+          name: 'menu',
+          id: 'menu',
+          transform: (post) => {
+            // do your transformation here...
+            return post
+          },
+          template: {
+            path: 'views/index.sgr',
+            output: (post) => { 
+              console.log(post)
+              return `index.html` 
+            }
+          }
+        }
+      ]
+    })
+  ],
+  devtool: 'source-map',
+  matchers: {
+    html: '*(**/)*.sgr',
+    css: '*(**/)*.sss'
+  },
+  ignore: ['**/*.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  reshape: htmlStandards({
+    locals
+  }),
+  postcss: cssStandards(),
+  babel: jsStandards()
+}
