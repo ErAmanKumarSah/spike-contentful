b230af8b9775290b923704262936d4aa1d990591
aman <amanjraj@gmail.com>
2017-05-16 17:09:22 +0530

    ava test is working for addition function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,3 @@
 console.log('you can use ES6 here...')
 require('./contentful_test.js')
+require('./script.js')
--- test.js
+++ test.js
@@ -1,15 +1,15 @@
 import test from 'ava'
-test('foo', t => {
-  t.pass()
+import sinon from 'sinon'
+
+var scriptjs = require('./src/script')
+
+test('Addition function ', async t => {
+  const promiseResolution = new Promise(resolve => resolve(scriptjs.sumT(2, 6)))
+  t.is(await promiseResolution, scriptjs.sumT(5, 3))
 })
 
-test('bar', async t => {
-  const bar = Promise.resolve('bar')
-  t.is(await bar, 'bar')
-})
-
+// contentful check on console
 var contentful = require('contentful')
-
 var client = contentful.createClient({
   space: 'pa6kchr1mcxx',
   accessToken: 'b2a023ce0af8f50240dce5cdfd5f8a9ab298f93bcaf24c57a9ab98e5191845c0'
@@ -20,12 +20,8 @@ client.getEntry('4VHtP1ZQT6qQaQiayA0YCI')
   // logs the entry metadata
   console.log(entry.sys)
 
-  // console.log('entry status success...')
+  console.log('entry status success...')
   // logs the field with ID title
   console.log(entry.fields.menuitem)
   console.log('menu success if its appear above')
-})
-
-test('menuTitle', t1 => {
-  t1.pass()
 })
